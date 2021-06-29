884ccede26b7c7f7d568f1d677326323f9a4779c
Rashid Derev <rashidderev@gmail.com>
2021-06-29 19:24:21 +0300

    1

--- src/Buttons.js
+++ src/Buttons.js
@@ -6,16 +6,16 @@ const Buttons = (props) => {
         return Object.entries(props.buttons).map(([key, value], i)=> {
         if(key === 'submit'){
             return (
-                <input className="btn formButton" type="submit" value={value[0]} style={{backgroundColor: value[1]}}></input>)
+                <input key={i} className="btn formButton" type="submit" value={value[0]} style={{backgroundColor: value[1]}}></input>)
                 }
             else if(key === 'cancel'){
             return (
-                <input className="btn formButton" type="reset" value={value[0]} style={{backgroundColor: value[1]}}></input>)
+                <input key={i} className="btn formButton" type="reset" value={value[0]} style={{backgroundColor: value[1]}}></input>)
                 }
             
             })
             }
-        }
+        } 
     
         const buttonsForm = getButtons()
 
